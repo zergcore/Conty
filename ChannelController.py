2979ddbaaf03dde9bddebcac66e3d34cf1d84721
zergcore <zaidi.ramos@gmail.com>
2022-06-24 15:23:04 -0400

    Update ChannelController.py

--- ChannelController.py
+++ ChannelController.py
@@ -59,11 +59,12 @@ class ChannelController:
 
     def saveUser(self, username, id):
         found=False
-        with open('users.txt') as f:
+        with open('users_waves.txt') as f:
             if str(id) in f.read():
                 found=True
             f.close()
         if not found:
+            #f = open("users_waves.txt", 'a', encoding='utf-8')
             f = open("users_waves.txt", 'a', encoding='utf-8')
             f.write(str(id) + " " + username + "\n")
             f.close()
@@ -71,16 +72,20 @@ class ChannelController:
             print("User " + username + " is already in the file")
     
     def recoverUsers(self):
-        file = open("users.txt",'r',encoding = 'utf-8')
+        file = open("users_waves.txt",'r',encoding = 'utf-8')
         for line in file:
             print(line)
             data=line.split(" ")
             self.users[int(data[0])]=data[1]
             print("User "+ data[1] +" was succesfully recovered")
+        file.close()
 
     def storeUsers(self, ids, usernames):
         for i in range(len(ids)):
             self.users[ids[i]]=usernames[i]
+            f = open("users_waves.txt", 'a', encoding='utf-8')
+            f.write(str(ids[i]) + " " + usernames[i] + "\n")
+            f.close()
     
     async def storeUser(self, client, channel, username):
         id = await self.getUserID(client, channel, username)
@@ -252,32 +257,32 @@ class ChannelController:
         chatters_ids=list(self.users.keys())
         df_chatters = self.filterData(chatters_ids,df)
         self.dataframes.append(df_chatters)
-        now=datetime.datetime.now()
-        df_chatters.to_csv(my_channel.title + "-" + now.strftime(r'%d-%m-%Y-%H-%S-%M') + '.csv', index=False,header=True)
-        print('File ' + my_channel.title + "-" + now.strftime(r'%d-%m-%Y-%H-%S-%M') + ' created succesfully')
+        #now=datetime.datetime.now()
+        #df_chatters.to_csv(my_channel.title + "-" + now.strftime(r'%d-%m-%Y-%H-%S-%M') + '.csv', index=False,header=True)
+        #print('File ' + my_channel.title + "-" + now.strftime(r'%d-%m-%Y-%H-%S-%M') + ' created succesfully')
 
 
 #projects_titles = ['StrongNode Edge','REBASE GG - OFFICIAL (CHAT)', 'Rango','STEPN Official English Group ♾', 'PropChain - Community']
-projects_ids = [1377587241, 1125567813, 1473496284, 1598082994, 1775645809] #SNE, REBL, Rango, Stepn, Prop
+#projects_ids = [1377587241, 1125567813, 1473496284, 1598082994, 1775645809] #SNE, REBL, Rango, Stepn, Prop
 #projects_names=['strongnodechat','REBASE_gg','rangoexchange','Wavescommunity', 'STEPNofficialex'] #Propchain isn't here
 #chatters_ids=[1157661659,259666186,1730962980,1779713175,1350200586]
 
 
-#projects_ids=[1112416724,1173560544,1446148003,1225614362] #Waves, Vires, WavesExchange, Neutrino Ids
-#wolf_ids=[1646032571,1783689378,1455185808,1664917820,1643813244] #WolfIDs
-#wolf_usernames=['kmsnowpls','NutiusMaximus', 'Mrfrogman00', 'fucktheteutonic', 'pleasestopthepain']
-#chatters_usernames=['artoriasfirstsign','AlPastor00','depravedmonolith','GateKeeppp','richgoldbergSon', 'feelingsomthing', 'ancapenjoyer'] #Saint and Username users
-chatters_usernames=['zergcore','Kim_1415', 'monkeylegionn', 'ivelosthopes', 'cryptoojamess', 'Longjyckle', 'gabygab_01', 'lightyagami2nd', 'annalissamlh', 'stitchbby', 'traderattempt', 'sttevewithTT', 'strawberrychann', 'muhfree57', 'captionerkk877', 'Pronx1', 'Greensoldier', 'itsjerkinsnotjerking', 'notanonion', 'lithooman58', 'fragger98', 'Saint1v', 'alejandraa', 'Melisassd', 'ericsims1', 'bonny377', 'jtrapiello', 'thebigched', 'cjone00', 'eduardanchang', 'Daddygod20', 'Bigboss30', 'dian377', 'jackbitcoin', 'thehugoneitor', 'Anwua4', 'Furbee123', 'Mynameisdogo', 'NicolloMo', 'LucazzZ123', 'CapitanCrunch12', 'uadondawest', 'Japan213', 'yungburrito2', 'cinnarolls9', 'mjape20', 'alvin365', 'Harry209', 'Heavyweight2', 'km2k15']
+projects_ids=[1112416724,1173560544,1446148003,1225614362] #Waves, Vires, WavesExchange, Neutrino Ids
+wolf_ids=[1646032571,1783689378,1455185808,1664917820,1643813244] #WolfIDs
+wolf_usernames=['kmsnowpls','NutiusMaximus', 'Mrfrogman00', 'fucktheteutonic', 'pleasestopthepain']
+chatters_usernames=['artoriasfirstsign','AlPastor00','depravedmonolith','GateKeeppp','richgoldbergSon', 'feelingsomthing', 'ancapenjoyer'] #Saint and Username users
+#chatters_usernames=['zergcore','Kim_1415', 'monkeylegionn', 'ivelosthopes', 'cryptoojamess', 'Longjyckle', 'gabygab_01', 'lightyagami2nd', 'annalissamlh', 'stitchbby', 'traderattempt', 'sttevewithTT', 'strawberrychann', 'muhfree57', 'captionerkk877', 'Pronx1', 'Greensoldier', 'itsjerkinsnotjerking', 'notanonion', 'lithooman58', 'fragger98', 'Saint1v', 'alejandraa', 'Melisassd', 'ericsims1', 'bonny377', 'jtrapiello', 'thebigched', 'cjone00', 'eduardanchang', 'Daddygod20', 'Bigboss30', 'dian377', 'jackbitcoin', 'thehugoneitor', 'Anwua4', 'Furbee123', 'Mynameisdogo', 'NicolloMo', 'LucazzZ123', 'CapitanCrunch12', 'uadondawest', 'Japan213', 'yungburrito2', 'cinnarolls9', 'mjape20', 'alvin365', 'Harry209', 'Heavyweight2', 'km2k15']
 
 client=Connect.client()
 ch=ChannelController()
 dialogs=ch.getDialogs(client)
 ch.storeChats(dialogs)
 ch.storeChannels(dialogs,projects_ids)
-#ch.storeUsers(wolf_ids,wolf_usernames)
+ch.storeUsers(wolf_ids,wolf_usernames)
 for user in chatters_usernames:
     with client:
-        client.loop.run_until_complete(ch.storeUser(client,'strongnodechat',user))
+        client.loop.run_until_complete(ch.storeUser(client,'Wavescommunity',user))
 ch.recoverUsers()
 for chnn in ch.channels:
     with client:
@@ -288,7 +293,7 @@ for chnn in ch.channels:
         #print(">> Done cancelling tasks")
         #asyncio.get_event_loop().stop()
         #client.loop.run_until_complete(ch.sleep(30))
-#ch.printStats(ch.getDataframes(),'Waves')
+ch.printStats(ch.getDataframes(),'Waves')
 
 
 '''data=ch.getHistory(client, InputPeerChannel(chnn.id, chnn.access_hash))
